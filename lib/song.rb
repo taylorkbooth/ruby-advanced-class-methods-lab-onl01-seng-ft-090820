95793adf71cfba35600676d67c993ad3c460a9c6
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 17:46:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,21 @@ end
       song.name = (name.split(" - ")[1].chomp(".mp3"))
       song.artist_name = (name.split(" - ")[0])
       song
-     
   end
+
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+
+
 
 end
