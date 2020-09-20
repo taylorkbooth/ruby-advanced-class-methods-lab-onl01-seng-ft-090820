4d60fd342d644cc88d44e5361db80173047d45c1
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 17:44:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,10 @@ end
       
     def self.new_from_filename(name)
       song = self.new
+      song.name = (name.split(" - ")[1].chomp(".mp3"))
+      song.artist_name = (name.split(" - ")[0])
+      song
      
+  end
 
 end
